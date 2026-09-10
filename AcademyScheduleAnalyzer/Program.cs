9c4a902cc54b00035198d2ad82a9cbf725c22e1d
Marwan Esam <marwanesam60@gmail.com>
2026-09-11 01:18:08 +0300

    feat: complete Part 1 starter data
    Refactor project directory structure
    Add .gitignore file
    Add starter data in Program.cs

---
+++ AcademyScheduleAnalyzer/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+  public static void Main(string[] args)
+  {
+    string[] sessionNames =
+    {
+      "C# Basics",
+      "Arrays",
+      "Functions",
+      "Date and Time",
+      "Exception Handling"
+    };
+
+    DateTime[] sessionDates =
+    {
+      new DateTime(2026, 9, 10, 18, 0, 0),
+      new DateTime(2026, 9, 13, 18, 0, 0),
+      new DateTime(2026, 9, 17, 18, 0, 0),
+      new DateTime(2026, 9, 20, 18, 0, 0),
+      new DateTime(2026, 9, 24, 18, 0, 0)
+    };
+
+    int[] sessionDurations =
+    {
+      180,
+      240,
+      180,
+      240,
+      180
+    };
+  }
+}
